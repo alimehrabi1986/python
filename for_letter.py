927d39fee73d8294acac63f11734528d1e6a07f6
Ali <alimehrabi1986@gmail.com>
2025-04-25 09:32:00 +0100

    added for_letter.py

---
+++ for_letter.py
@@ -0,0 +1,6 @@
+name = "Ali7032"
+for character in name:
+    if character.isalpha():
+        print(f"{character} is a letter")
+    elif character.isdigit():
+        print(f"{character} is a digit")
